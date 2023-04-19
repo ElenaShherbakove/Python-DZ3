872692e8bec450ad39230fe117ad522a73fcb307
Elena <Shherbakove@yandex.ru>
2023-04-19 20:35:00 +0300

    Домашка к 3 семинару

---
+++ Task18.py
@@ -0,0 +1,28 @@
+# Задача 18:
+# Требуется найти в списке A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в списке.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+#
+# *Пример:*
+#
+# 5
+#     1 2 3 4 5
+#     6
+#     -> 5
+import random
+n = int(input("Введите длину списка "))
+list_test = []
+for i in range(n):
+    list_test.append(random.randint(-n, n+n//2))
+print(list_test)
+x = int(input("Введите число "))
+val = list_test[0]
+max_val = abs(x - list_test[0])
+set_test = set(list_test)
+for i in set_test:
+    t = abs(x - i)
+    if t < max_val:
+        max_val = t
+        val = i
+
+print(f"-> {val}")
